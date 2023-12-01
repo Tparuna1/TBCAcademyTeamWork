99378101eefe2a5545ac59978c170f5b9371029a
slapiashvili <sslapiashvili@gmail.com>
2023-12-01 16:35:05 +0400

    add view model viewmodel

--- EcoTrackerApp/EcoTrackerApp/SpeciesPage/SpeciesPageModel.swift
+++ EcoTrackerApp/EcoTrackerApp/SpeciesPage/SpeciesPageModel.swift
@@ -7,10 +7,80 @@
 
 import Foundation
 
-struct SpeciesPageModel {
+struct SpeciesDefaultPhoto: Decodable {
+    let attribution: String?
+    let url: String?
+
+    enum CodingKeys: String, CodingKey {
+        case attribution = "attribution"
+        case url = "url"
+    }
+}
+
+struct SpeciesTaxon: Decodable {
+    let name: String
+    let defaultPhoto: SpeciesDefaultPhoto?
+    let attribution: String?
+    let wikipediaURL: String?
+
+    enum CodingKeys: String, CodingKey {
+        case name = "name"
+        case defaultPhoto = "default_photo"
+        case attribution = "attribution"
+        case wikipediaURL = "wikipedia_url"
+    }
+}
+
+struct SpeciesPageModel: Decodable {
     let speciesName: String
-    let speciesDiscoveryInfo: String
-    let speciesUploaderName: String
     let speciesWikipediaLink: String?
     let speciesImageUrl: String?
+    let speciesDefaultPhoto: SpeciesDefaultPhoto?
+
+    enum CodingKeys: String, CodingKey {
+        case speciesName
+        case speciesWikipediaLink
+        case speciesImageUrl
+        case speciesDefaultPhoto = "defaultPhoto"
+    }
 }
+
+struct AutocompleteResponse: Decodable {
+    let totalResults: Int
+    let results: [AutocompleteResult]
+
+    enum CodingKeys: String, CodingKey {
+        case totalResults = "total_results"
+        case results
+    }
+}
+
+struct AutocompleteResult: Decodable {
+    let id: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+    }
+}
+
+struct SpeciesCountResult: Decodable {
+    let count: Int
+    let taxon: SpeciesTaxon
+
+    enum CodingKeys: String, CodingKey {
+        case count
+        case taxon
+    }
+}
+
+struct SpeciesCountResponse: Decodable {
+    let totalResults: Int
+    let results: [SpeciesCountResult]
+
+    enum CodingKeys: String, CodingKey {
+        case totalResults = "total_results"
+        case results
+    }
+}
+
+
